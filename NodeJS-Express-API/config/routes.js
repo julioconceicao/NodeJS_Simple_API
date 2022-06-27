2974c5cb13745f1a167cd46496c5d7961fc92109
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-27 04:34:23 -0300

    :tada: First Commit [Full Project]

---
+++ NodeJS-Express-API/config/routes.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const routes = express.Router()
+
+//Fake Database items
+let db = [
+    { '1': { Name: 'Client 1', Age: '20' } },
+    { '2': { Name: 'Client 2', Age: '20' } },
+    { '3': { Name: 'Client 3', Age: '20' } },
+]
+
+
+// Get data
+routes.get('/', (req, res) => {
+    return res.json(db)
+})
+
+// Insert data
+routes.post('/add', (req, res) => {
+    const body = req.body
+
+    if (!body)
+        return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+
+})
+
+routes.delete('/:id', (req, res) => {
+    const id = req.params.id
+    let newDB = db.filter(item => {
+        if(!item[id])
+        return item
+    })
+db = newDB
+
+    return res.send(newDB)
+})
+
+
+
+module.exports = routes
+
---
+++ NodeJS-Express-API/index.js
@@ -0,0 +1,21 @@
+
+//Set the libraries const's
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+const { route } = require('./config/routes')
+
+const app = express()
+
+//Use the libraries at the app
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+app.listen(3002, () => {
+    console.log(`Express started at http://localhost:3002`)
+})
